aaf037320604a378276d1c0951f28f26a39d0a52
nehemiasml <maygamerfernando10@gmail.com>
2025-09-04 12:34:10 -0500

    Asistencia

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/control/MainguiController.java
+++ Asistencia/src/main/java/pe/edu/upeu/asistencia/control/MainguiController.java
@@ -19,28 +19,39 @@ import java.util.Map;
 public class MainguiController {
 
     @FXML
-    private MenuItem menuItem1, menuItem2;
+    private MenuItem menuItem1, menuItem2, menuItem3,  menuItem4;
+
     @FXML
     private MenuBar menuBar;
 
     @FXML
     private TabPane tabPane;
+
     @Autowired
     private ApplicationContext context;
+
     @FXML
-    public void initialize() {
-        MenuItemListener miL=new MenuItemListener();
+    public void initialize(){
+        MenuItemListener miL= new MenuItemListener();
         menuItem1.setOnAction(miL::handle);
         menuItem2.setOnAction(miL::handle);
+        menuItem3.setOnAction(miL::handle);
+        menuItem4.setOnAction(miL::handle);
+
+
+
     }
-    class MenuItemListener{
+    class MenuItemListener {
         Map<String, String[]> menuConfig=Map.of(
-                            "menuItem1",new String[]{"/fxml/main_participante.fxml","Participantes","T"},
-                            "menuItem2",new String[]{"/fxml/login.fxml","Salir","C"}
-                                );
+                "menuItem1", new String[]{"/fxml/main_participante.fxml","Participante","T"},
+                "menuItem2", new String[]{"/fxml/login.fxml","Salir","C"},
+                "menuItem3", new String[]{"/fxml/main_asistencia.fxml","Asistencia","T"},
+                "menuItem4", new String[]{"/fxml/main_asistencia.fxml","Asistencia","T"}
+
+        );
 
         public void handle(ActionEvent e) {
-            String id= ( (MenuItem) e.getSource() ).getId();
+            String id=( (MenuItem) e.getSource() ).getId();
             if(menuConfig.containsKey(id)){
                 String[] mi=menuConfig.get(id);
                 if(mi[2].equals("C")){
@@ -54,28 +65,33 @@ public class MainguiController {
 
         public void abrirArchivoFXML(String filename, String tittle){
             try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(filename));
-            loader.setControllerFactory(context::getBean);
-            Parent root = loader.load();
 
-            ScrollPane scrollPane = new ScrollPane(root);
-            scrollPane.setFitToWidth(true);
-            scrollPane.setFitToHeight(true);
-            Tab newTab = new  Tab(tittle, scrollPane);
-            tabPane.getTabs().clear();
-            tabPane.getTabs().add(newTab);
+                FXMLLoader loader = new FXMLLoader(getClass().getResource(filename));
+                loader.setControllerFactory(context::getBean);
+                Parent root = loader.load();
+
+                ScrollPane scrollPane = new ScrollPane(root);
+                scrollPane.setFitToWidth(true);
+                scrollPane.setFitToHeight(true);
+                Tab newTab = new Tab(tittle, scrollPane);
+                tabPane.getTabs().clear();
+                tabPane.getTabs().add(newTab);
+
+
 
             }catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
+
         }
+
+
     }
 
-    class MenuListener{
+    class Menuistener{
         public void handle(Event e) {
 
         }
     }
-
 
 }
