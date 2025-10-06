b253d3bced5e94034251e0795d59e11883e5ece0
Asistencia <you@example.com>
2025-10-05 22:32:22 -0500

    Asistencia

--- ConceptosPOO/src/main/java/pe/edu/upeu/abspolimorfismo/Animal.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.abspolimorfismo;
-
-public abstract class Animal {
-   abstract void emitirSonido();
-
-   void dormir(){
-       System.out.println("Zzz...zzz..zz.");
-   }
-}
--- Venta/src/main/java/pe/edu/upeu/venta/clienteapp/Aplicacion.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.venta.clienteapp;
-
-public class Aplicacion {
-    public static void main(String[] args) {
-        System.out.println("Iniciando  DMP");
-        AsistenciaAplicacion.main(args);
-
-    }
-}
