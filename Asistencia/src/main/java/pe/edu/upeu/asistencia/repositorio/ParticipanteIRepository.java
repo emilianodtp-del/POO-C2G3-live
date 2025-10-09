bb9f89fb1bf018f4a81aa42a7a85824933769f03
nehemiasml <maygamerfernando10@gmail.com>
2025-10-09 08:36:43 -0500

    Proyecto Ventas

--- Asistencia/src/main/java/pe/edu/upeu/asistencia/repositorio/ParticipanteIRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upeu.asistencia.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upeu.asistencia.modelo.Participante;
-//@Repository
-public interface ParticipanteIRepository extends JpaRepository<Participante, String> {
-}
--- ConceptosPOO/src/main/java/pe/edu/upeu/polimorfismo/Gato.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upeu.polimorfismo;
-
-public class Gato extends Animal {
-    @Override
-    void sonidoAnimal() {
-        System.out.println("Meu....meu...");
-    }
-}
